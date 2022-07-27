af0901cefaded9233ed73aa4fb71d7089e21d096
Matthias Hecht <dev@hecht.space>
2022-07-27 17:34:06 +0200

    ci: zipping

--- pkg.sh
+++ pkg.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
-out.zip
-zip -r out.zip package.json package-lock.json node_modules types/package.json types/dist package.json server/dist package.json web/dist
+rm -r eEntry
+mkdir eEntry
+cp package.json eEntry
+cp package-lock.json eEntry
+
+cp -r node_modules eEntry
+
+# TYPES
+mkdir -p eEntry/types/dist
+cp types/package.json eEntry/types
+cp -r types/dist/* eEntry/types/dist
+
+# SERVER
+mkdir -p eEntry/server/dist
+cp server/package.json eEntry/server
+cp -r server/dist/* eEntry/server/dist
+
+# WEB
+mkdir -p eEntry/web/dist
+cp web/package.json eEntry/web
+cp -r web/dist/* eEntry/web/dist
